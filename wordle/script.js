c90e57188c2a4d89894869a04c7e5b30cf87bbbc
Yousuf Uyghur <yousufuighur@yahoo.com>
2025-05-06 17:12:13 +0500

    enter functionality corrected, backspace left

--- wordle/script.js
+++ wordle/script.js
@@ -1,5 +1,10 @@
 const container = document.getElementById("grid");
-let boxIdCounter = 0;
+let boxIdCounter = 0; // counter to create the grod 
+let boxCounter = 0; // counter to manage and update boxes in handle key events 
+const WORD_LENGTH = 5;
+const TOTAL_ROWS = 6;
+let currentRow = 0;
+
 
 // create the wordle grid boxes
 function createGrid(grid, rows, cols) {
@@ -9,7 +14,7 @@ function createGrid(grid, rows, cols) {
 
         for (let j = 0; j < cols; j++) {
             const box = document.createElement("div");
-            box.id = boxIdCounter;
+            box.id = `${i * cols + j}`;
             box.classList.add("box");
             row.appendChild(box);
             boxIdCounter += 1;
@@ -18,7 +23,7 @@ function createGrid(grid, rows, cols) {
     }
 }
 
-createGrid(container, 6, 5);
+createGrid(container, TOTAL_ROWS, WORD_LENGTH);
 
 // here starts the api work and fetching
 const WORD_URL = "https://words.dev-apis.com/word-of-the-day?random=1";
@@ -30,52 +35,71 @@ function isLetter(letter) {
 }
 
 // function to update the characters in boxes 
-let boxCounter = 0;
 
 async function postWord(word) {
     const promise = await fetch(VALIDATE_URL, {
-        method: "POST", 
-        body: JSON.stringify({"word": word})
-    }); 
+        method: "POST",
+        body: JSON.stringify({ "word": word })
+    });
 
     const response = await promise.json();
-    const valid = response.validWord; 
-
-    return valid; 
+    return response.validWord;
 }
 
-function validateWord(word) {
-    const validation = postWord(word);
-
-    if (validation === true) {
-        console.log("it works");
-    } else { 
-        console.log("doesn't work");
+function updateBox(value) {
+    if (boxCounter < TOTAL_ROWS * WORD_LENGTH) {
+        const box = document.getElementById(boxCounter);
+        box.innerHTML = value.toUpperCase();
+        boxCounter += 1;
     }
 }
 
-function updateBox(key, event) {
-    let box = document.getElementById(boxCounter);
-    
-    if (boxCounter < 30) {
-        if (((boxCounter) % 5) === 0 && boxCounter != 0) {
-            if (key !== "Enter") {
-                alert("please input enter first");
-                event.preventDefault();
-            } else if (key === "Enter") {
-                validateWord();
-            }
+function handleBackspace() {
+    // handles the removal of last word typed, doesn't let go out of row 
+    if (boxCounter > 0 && boxCounter % WORD_LENGTH !== 0) {
+        boxCounter--;
+        const box = document.getElementById(boxCounter);
+        box.innerHTML = "";
+    }
+}
+
+function getWord() {
+    let word = "";
+    for (let i = boxCounter - WORD_LENGTH; i < boxCounter; i++) {
+        const char = document.getElementById(i);
+        word += char.innerText;
+    }
+    return word.toLowerCase();
+}
+
+async function handleEnter() {
+    if (boxCounter === WORD_LENGTH * (currentRow + 1)) {
+        const word = getWord();
+        const valid = await postWord(word);
+        console.log(word)
+
+        if (valid) {
+            currentRow++;
+            console.log("congratulations the word is correct");
         } else {
-            if (isLetter(key)) {
-                box.innerHTML = key.toUpperCase();
-                console.log(boxCounter);
-                boxCounter += 1;
-            }
+            console.log("wrong word, please check again");
         }
+    } else {
+        console.log("COMPLETE THE WORD");
+        return;
     }
 }
 
+document.addEventListener("keydown", async (e) => {
+    const key = e.key;
 
-document.addEventListener("keydown", (e) => {
-    updateBox(e.key, e);
+    if (isLetter(key)) {
+        if (boxCounter < TOTAL_ROWS * WORD_LENGTH && boxCounter < (currentRow + 1) * WORD_LENGTH) {
+            updateBox(key, e);
+        }
+    } else if (key === "Enter") {
+        await handleEnter();
+    } else if (key === "Backspace") {
+        handleBackspace();
+    }
 });
